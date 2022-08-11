0edc6dea4ce7cb72e64baf4ef9bb65b800e8b1f0
minwyang <minwyang@blackberry.com>
2022-08-11 13:47:34 -0400

    change ranges

--- python_scripts/password_generator.py
+++ python_scripts/password_generator.py
@@ -2,5 +2,5 @@ import string
 from random import *
 
 format   = string.ascii_letters + string.punctuation + string.digits
-password = "".join(choice(format) for x in range(randint(1,16)))
+password = "".join(choice(format) for x in range(randint(10,16)))
 print(password)
