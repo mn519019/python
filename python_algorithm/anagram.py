9e55d1d99d703c117db33313c4bd2591e1fe5d80
Rick Minwoo Yang <62801422+mn519019@users.noreply.github.com>
2022-10-14 10:18:34 -0300

    Update anagram.py

--- python_algorithm/anagram.py
+++ python_algorithm/anagram.py
@@ -1,4 +1,5 @@
-# We are given two strings like "cinema" and "iceman" as inputs. Can you write a method isAnagram(str1, str2) that will return true or false depending on whether the strings are anagrams of each other?
+# We are given two strings like "cinema" and "iceman" as inputs. Can you write a method isAnagram(str1, str2) 
+# that will return true or false depending on whether the strings are anagrams of each other?
 def is_anagram(str1, str2):
     ## fill this in
     l1 = list(str1.lower())
