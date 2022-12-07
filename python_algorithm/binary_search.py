cf12e4cc80e0956e2b2d3687cbbc95806143ca8d
Rick Minwoo Yang <62801422+mn519019@users.noreply.github.com>
2022-12-06 20:50:29 -0400

    Create binary_search.py

---
+++ python_algorithm/binary_search.py
@@ -0,0 +1,29 @@
+list1 = ['Alice','Kim','Bob','Corner','Minwoo','Domino','Elice','Frank'
+,'Gong','Hay','Jason','Lion','Nan']
+
+sortlist = sorted(list1)
+# Binary Search Approach
+def binary_search(list, target):
+    left = 0
+    right = len(list) -1 
+    while(left<=right):
+        mid = (left+right) // 2
+        if list[mid] == target:
+            return list[mid]
+        elif list[mid] < target:
+            left = mid + 1 
+        else:
+            right = mid -1
+pass
+        
+# print(binary_search(sortlist,'Kim'))
+
+# Linear Search Approach
+# def linear_search(list,target):
+#     for i, x in enumerate(list):
+#         if x == target:
+#             return str(x) +" has been found on index " + str(i)
+#             break
+        
+# print(linear_search(sortlist,"Kim"))
+
