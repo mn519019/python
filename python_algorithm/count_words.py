f6b5d72c2c970c51221e9e0f7ac30f6e1aea4afe
minwoo <mn519019@dal.ca>
2023-01-03 19:55:31 -0400

    count word python file

---
+++ python_algorithm/count_words.py
@@ -0,0 +1,16 @@
+# Wrtie a python program to count the frequency of words appearing in a string
+# Example: Sheena loves eating apple and mango. Her sister also loves eating aplen and mango 
+
+str1 = "Sheena loves eating apple and mango. Her sister also loves eating apple and mango"
+x = str1.split(" ")
+
+def frequent_word():
+    duplicates=[]
+    myfreq = [x.count(p) for p in x]
+    return dict(zip(x,myfreq))
+
+print(frequent_word())
+
+## Output 
+# {'Sheena': 1, 'loves': 2, 'eating': 2, 'apple': 2, 'and': 2, 
+# 'mango.': 1, 'Her': 1, 'sister': 1, 'also': 1, 'mango': 1}
