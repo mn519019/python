6b9e64dfe971ee4de6fbafc04a53e4e5de7da5a3
minwoo <mn519019@dal.ca>
2023-01-09 15:24:38 -0400

    update

---
+++ python_algorithm/find_paris_with_sum.py
@@ -0,0 +1,31 @@
+# Find out pairs with given sum value of an array, Adopting a binary search approach and we can solve this question
+# arr = [5,7,4,3,9,8,19,21] and sum= 17 
+
+def find_target_sum(list,target):
+    list.sort()
+    print(list)
+    left = 0
+    right = len(list) -1
+    
+    for data in list:
+        sum = list[left] + list[right]
+        if sum > target:
+            right = right - 1
+        elif sum < target:
+            left = left + 1
+        elif sum == target:
+            print("matching paris are: ",list[left],"&",list[right])
+            left = left + 1
+            right = right - 1
+        else:
+            print("no matching pairs are found to get ", target)  
+    return " "
+
+
+list1 = [0,5,7,4,3,9,8,21,19,17]
+print(find_target_sum(list1,17))
+
+# list1 = [5,7,4,3,9,8,21,19]
+
+# for i in list1:
+#     print(i)
