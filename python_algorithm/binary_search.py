e03d6e881fba8ed23fb40b64fb1d8e83c9c5a8a4
minwoo <mn519019@dal.ca>
2023-01-11 10:59:46 -0400

    update

--- python_algorithm/binary_search.py
+++ python_algorithm/binary_search.py
@@ -2,28 +2,35 @@ list1 = ['Alice','Kim','Bob','Corner','Minwoo','Domino','Elice','Frank'
         ,'Gong','Hay','Jason','Lion','Nan']
 
 sortlist = sorted(list1)
+print(sortlist)
+
 # Binary Search Approach
-def binary_search(list, target):
+def binary_search2(list,target):
     left = 0
-    right = len(list) -1 
-    while(left<=right):
+    right = len(sortlist) - 1 
+    while left<=right:
         mid = (left+right) // 2
-        if list[mid] == target:
-            return list[mid]
-        elif list[mid] < target:
-            left = mid + 1 
-        else:
+        if target < list[mid]:
             right = mid -1
-    return -1
+        elif target > list[mid]:
+            left = mid + 1
+        else:
+            return list[mid] + " has been found on index " + str(mid)
+    return ""
+
+
         
-print(binary_search(sortlist,'Kim'))
+print(binary_search2(sortlist,'Kim'))
+
+# Average Time complexity: O(logn)
+# The best case: O(1)
 
 # Linear Search Approach
-def linear_search(list,target):
-    for i, x in enumerate(list):
-        if x == target:
-            return str(x) +" has been found on index " + str(i)
-            break
+# def linear_search(list,target):
+#     for i, x in enumerate(list):
+#         if x == target:
+#             return str(x) +" has been found on index " + str(i)
+#             break
         
-print(linear_search(sortlist,"Kim"))
+# print(linear_search(sortlist,"Kim"))
 
