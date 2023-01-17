3756321cb3f335c4ec18f4f3d723651e1ef741f1
minwoo <mn519019@dal.ca>
2023-01-17 13:19:17 -0400

    updating code to pass variables

--- python_scripts/docker_image.py
+++ python_scripts/docker_image.py
@@ -1,22 +1,31 @@
+import sys
 import docker
 # Docker module needs to be installed to use Python SDK
 
 class docker_controller:
-    def signin(self,str1,str2,str3):
+    def __init__(self,username,password,dock_img):
+        self.username = None
+        self.password = None
+        self.dock_img = None
+
+    def signin(self, name, pswd):
         try:
             client = docker.from_env()
-            client.login(username=str1, password=str2)
+            client.login(username=self.username, password=self.password)
             print( "Successfully sign-in Docker Registry \n")
-            image = client.images.pull(str3)
-            print("Avialbe Docker Images from your hub: \n")
-            for image in client.images.list():
-                container_id = image.id
-                print(client.images.get(image.id))
         except:
             print("Failed to sign-in docker registry")
-    
-    def docker_pull():
+            sys.exit(1)
+
+    def docker_pull(self, docker_img):
         print("docker pull")
+        client = docker.from_env()
+        image = client.images.pull(docker_img)
+        print("Avialbe Docker Images from your hub: \n")
+        for image in client.images.list():
+            container_id = image.id
+            print(client.images.get(image.id))
+
     def docker_push():
         print("docker push")
 
@@ -25,9 +34,10 @@ if __name__ == "__main__" :
         str1 = raw_input("Please enter your docker ID: \n")
         str2 = raw_input("please enter your docker password: \n")
         str3 = raw_input("please enter name of the docker image: \n")
-        controller = docker_controller()
-        controller.signin(str1,str2,str3)
-        out = raw_input("Type q to exit the loop: \n")
+        controller = docker_controller(str1,str2,str3)
+        controller.signin(str1,str2)
+        controller.docker_pull(str3)
+        out = raw_input("Type \"q\" to exit the loop, otherwise type \"y\" \n")
         if out == 'q':
             break
     print("Docker pulling script is terminated.")
