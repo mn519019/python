71a2b4647cdb3c97b68f9705a0e2f075752fcd46
minwoo <mn519019@dal.ca>
2023-01-17 10:46:15 -0400

    update

--- python_scripts/docker_image.py
+++ python_scripts/docker_image.py
@@ -2,21 +2,28 @@ import docker
 # Docker module needs to be installed to use Python SDK
 
 class docker_pull:
-    def signin(self,str1,str2):
+    def signin(self,str1,str2,str3):
         try:
             client = docker.from_env()
             client.login(username=str1, password=str2)
             print( "Successfully sign-in Docker Registry \n")
-            #image = client.images.pull("alpine")
+            image = client.images.pull(str3)
             print("Avialbe Docker Images from your hub: \n")
             for image in client.images.list():
-                print(image.id)
+                container_id = image.id
+                print(client.images.get(image.id))
         except:
             print("Failed to sign-in docker registry")
 
 
 if __name__ == "__main__" :
-    str1 = raw_input("Please enter your docker ID: \n")
-    str2 = raw_input("please enter your docker password: \n")
-    dockerpull = docker_pull()
-    dockerpull.signin(str1,str2)
+    while True:
+        str1 = raw_input("Please enter your docker ID: \n")
+        str2 = raw_input("please enter your docker password: \n")
+        str3 = raw_input("please enter name of the docker image: \n")
+        dockerpull = docker_pull()
+        dockerpull.signin(str1,str2,str3)
+        out = raw_input("Type q to exit the loop: \n")
+        if out == 'q':
+            break
+    print("Docker pulling script is terminated.")
