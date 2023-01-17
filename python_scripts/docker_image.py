6351147d39f176bf32e65042d318f45497a8d997
minwoo <mn519019@dal.ca>
2023-01-17 15:09:01 -0400

    docker rmi function

--- python_scripts/docker_image.py
+++ python_scripts/docker_image.py
@@ -31,24 +31,31 @@ class docker_controller:
         for line in client.images.push(docker_img,tag="latest",stream=True, decode=True):
             print(line)
 
-    ## Docker Remove Image 
-    ## TBC
-    
+    def docker_remove(self,docker_img):
+        client = docker.from_env()
+        rmi = client.images.remove(docker_img)
+        print("Avialbe Docker Images from your hub: \n")
+        for image in client.images.list():
+            container_id = image.id
+            print(client.images.get(image.id))
 
 if __name__ == "__main__" :
     push = {'push','Push', 'PUSH'}
     pull = {'pull','Pull', 'PULL'}
+    remove = {'remove', 'Remove', 'REMOVE'}
     while True:
         str1 = raw_input("Please enter your docker ID: \n")
         str2 = raw_input("please enter your docker password: \n")
         str3 = raw_input("please enter name of the docker image: \n")
         controller = docker_controller(str1,str2,str3)
         controller.signin(str1,str2)
-        option = raw_input("Type \"push or pull\" to complete the operation: \n ")
+        option = raw_input("Type \"push or pull or remove\" to complete the operation: \n ")
         if option in push:
             controller.docker_push(str3)
         elif option in pull: 
             controller.docker_pull(str3)
+        elif option in remove:
+            controller.docker_remove(str3)
         else:
             print("Required input is not typed.")
             sys.exit(1)
